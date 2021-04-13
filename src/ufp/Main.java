85f704cf33a558369e59df131e999489057e7bbe
rpedrosg1 <rperosg1@hotmail.com>
2021-04-13 10:47:53 +0100

    Começo do desastre

--- src/ufp/Main.java
+++ src/ufp/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("ada");
+        System.out.println("Diogo");
+        System.out.println("anda");
+        System.out.println("la");
+        System.out.println("meu");
     }
 }
