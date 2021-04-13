67de90e61eb84b7d20f5c31480eef8c189bf57d7
rpedrosg1 <rperosg1@hotmail.com>
2021-04-13 11:02:48 +0100

    Começo do desastre

--- src/ufp/Main.java
+++ src/ufp/Main.java
@@ -3,11 +3,16 @@ package ufp;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Diogo");
         System.out.println("anda");
         System.out.println("la");
         System.out.println("meu");
         System.out.println();
+        enum geocacheDiff {FACIL, MEDIO, DIFF}
+        String dificuldade = "FACIL";
+        //Geocache geo = "medio"
+        //Geocache geo = Geocache.FACIL;
+
     }
 }
