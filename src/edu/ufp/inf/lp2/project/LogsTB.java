6439de362c5a64d4b7cb78b74645861feccffcda
diferreira2000 <diogoferreira299@gmail.com>
2021-05-01 01:40:38 +0100

    fiz o toostring dos logs TB

--- src/edu/ufp/inf/lp2/project/LogsTB.java
+++ src/edu/ufp/inf/lp2/project/LogsTB.java
@@ -5,25 +5,41 @@ import edu.princeton.cs.algs4.BST;
 import edu.princeton.cs.algs4.RedBlackBST;
 
 import java.util.Vector;
-
+import static edu.ufp.inf.lp2.project.Admin_User.userST;
 public class LogsTB {
-  String nome_cache;//pode ser a cache onde foi deixada ou q acabou de ser retirado
-  String id_user;//pode ser o user q deixou ou q tirou
+  String nome_cache;//cache onde ele ta atualmente se for null n ta numa cache
+  String id_user;//user onde ele ta atualmente se for null n ta num user
   Date data;
-  Cache c;//cache onde ele ta atualmente se for null n ta numa cache
-  Premium_User u;//user onde ele ta atualmente se for null n ta num user
+  Cache c;
+  Premium_User u;
 
   boolean missao_concluida;
 
 
-  public LogsTB(String nome_cache,String id_user, Date data,Cache c,Premium_User u) {
-    this.nome_cache=nome_cache;
-    this.id_user=id_user;
+  public LogsTB(String nome_cache, String id_user, Date data, Cache c, Premium_User u) {
+    this.nome_cache = nome_cache;
+    this.id_user = id_user;
     this.data = data;
-    this.c=c;
-    this.u=u;
-    missao_concluida=false;
+    this.c = c;
+    this.u = u;
+    missao_concluida = false;
   }
+
+  @Override
+  public String toString() {
+    if (u == null) {
+      return "O user " + userST.get(id_user).nome + " deixou este Travel Bug na Cache "+
+               nome_cache + "\n" +
+              "Na data: " + data.toString() +
+              ", missao_concluida=" + missao_concluida;
+    }
+    return "O user " + userST.get(id_user).nome + " retirou este Travel Bug da Cache "+
+            nome_cache + "\n" +
+            "Na data: " + data.toString() +
+            ", missao_concluida=" + missao_concluida;
+  }
+
+
 
 
 }
